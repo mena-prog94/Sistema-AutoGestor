e8b5d59dad67915419034ae6da02fa35d2633535
Jovanny <menajovanny9@gmail.com>
2026-07-11 13:13:22 -0400

    Forzar permisos nativos de cleartext y servidores en Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'sistema-auto-gestor',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    cleartext: true,
+    allowNavigation: ['192.168.1.6']
+    }
 };
 
 export default config;
